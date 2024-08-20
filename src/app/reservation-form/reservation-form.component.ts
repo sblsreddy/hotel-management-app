ccd1081568c8f10ca918848166bc2590d41667b6
sblsreddy <r14141@gmail.com>
2024-08-20 14:44:20 -0500

    Step Reactive Validation on fields

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -3,19 +3,32 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
-  styleUrls: ['./reservation-form.component.css']
+  styleUrls: ['./reservation-form.component.css'],
 })
 export class ReservationFormComponent implements OnInit {
 
-  constructor() { }
+  // Dependency injection , when instance is created the contrcutor will be invoked
+  constructor(private formBuilder: FormBuilder) {
 
+  }
+
+// Life cycle hook when component is initialized
   ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', Validators.required]
+      
+    })
   }
 
   reservationForm: FormGroup = new FormGroup({});
 
-onSubmit(){
-  
-}
+  onSubmit() {
+    if(this.reservationForm.valid){
 
+    }
+  }
 }
